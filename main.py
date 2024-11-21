104f46f97913d2fc6c25f292aa52f2f1a441b549
1982739 <jos.nordenflycht@uc.cl>
2024-11-21 12:20:59 -0300

    chato

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ P, Z, Q, T, B, B_pq, γ, γ_zq, d_zp, v_p, C_z, f_qj, Φ_q, K, h_z, T_max = carg
 #alpha_pzt
 alpha = model.addVars(P, Z, T, vtype = GRB.BINARY, name = "alpha") #Indica si la persona p comienza a evacuar hacia la zona segura z en el instante de tiempo t.
 #X_zt
-X = model.addVars(Z, T, vtype = GRB.INTEGER, name = "X", lb=0) #Cantidad de personas en la zona segura z en el tiempo t.
+x = model.addVars(Z, T, vtype = GRB.INTEGER, name = "X", lb=0) #Cantidad de personas en la zona segura z en el tiempo t.
 #w_pqt
 w = model.addVars(P, Q, T, vtype = GRB.BINARY, name = "w") #Indica si la persona p se encuentra  en un cuadrante q en el tiempo t.
 #theta_pqjt
@@ -21,7 +21,7 @@ model.update()
 
 ### FO ###
 
-objetivo = quicksum(X[z, T_max] for z in Z)
+objetivo = quicksum(x[z, T_max] for z in Z)
 
 model.setObjective(objetivo, GRB.MAXIMIZE)
 
@@ -35,13 +35,13 @@ model.addConstrs((quicksum(alpha[p, z, t] for z in Z for t in T) <= 1
                  name="r_2")
 
 #R3 / Restriccion de valor de alpha
-model.addConstrs((alpha[p, z, t] <= (w[p, q, t] + γ_zq[z, q]) / 2
+model.addConstrs((2*alpha[p, z, t] <= w[p, q, t] + γ_zq[z, q]
                   for p in P for q in Q for t in T for z in Z),
                  name="r_3")
 
 #R4 / Restriccion de cambio de cuadrante
 model.addConstrs((quicksum(theta[p, q, j, t] for t in T) <= 1 - B_pq[p, j]
-                  for p in P for q in Q for j in Q for t in T),
+                  for p in P for q in Q for j in Q if j != q),
                  name="r_4")
 
 #R5 / Condicion inicial de cuadrante
@@ -66,7 +66,7 @@ model.addConstrs((w[p,q,t] >= quicksum(theta[p,q,j,t] for j in Q if j != q)
                  name="r_8")
 
 #R9 / Cantidad minima a evacuar
-model.addConstr((quicksum(X[z,T_max]for z in Z) >= K),
+model.addConstr((quicksum(x[z,T_max]for z in Z) >= K),
                  name="r_9")
 
 #R10 / Restriccion de zonas afectadas por tsunami **
@@ -81,19 +81,19 @@ model.addConstrs((quicksum(w[p,q,t] for p in P) <= quicksum(C_z*γ_zq[z,q] for z
                  name="r_11")
 
 #R12 / Restriccion de inventario de personas de la zona segura
-model.addConstrs((X[z,t] == X[z,t-1]
-                  + quicksum(alpha[p, z, t - d_zp[z, p] // v_p[p] - int(1.3 * h_z[z])]for p in P if 2 <= t - d_zp[z, p] // v_p[p] - int(1.3 * h_z[z]) <= max(T))
+model.addConstrs((x[z,t] == x[z,t-1]
+                  + quicksum(alpha[p, z, t - d_zp[z, p] // (v_p[p]*60) - int(1.3 * h_z[z])]for p in P if 2 <= t - d_zp[z, p] // (v_p[p]*60) - int(1.3 * h_z[z]) <= max(T))
                   for z in Z for t in T if t > min(T)),
                  name="r_12")
 
 #R13 / Condicion inicial de inventario de la zona segura
-model.addConstrs((X[z,1] == 0
+model.addConstrs((x[z,1] == 0
                   for z in Z),
                  name="r_13")
 
 #R14 / capacidad de la zona segura
-model.addConstrs((X[z,T_max] <= C_z
-                  for z in Z),
+model.addConstrs((x[z,t] <= C_z
+                  for z in Z for t in T),
                  name="r_14")
 
 model.optimize()
@@ -102,4 +102,5 @@ try:
     print("Objetivo:", model.ObjVal)
 except:
     print("No se encontró solución")
+    model.write("model.lp")
 
